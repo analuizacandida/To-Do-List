49de315076d4bca155959f0996b68643c56ca190
analuizacandida <analuizacandidavc@gmail.com>
2025-05-11 09:04:23 -0300

    enviar arquivos

--- app/src/main/java/com/example/trekgo/home.kt
+++ app/src/main/java/com/example/trekgo/home.kt
@@ -7,7 +7,7 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class home: AppCompatActivity() {
-
+//aqui
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home);
